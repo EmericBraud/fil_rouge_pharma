ed4131cbf35a35d16a3cf0145ac2d20db20b8a11
killian-bchr <killian.bouchereau@outlook.fr>
2026-02-09 10:00:46 +0100

    add ordonancement

---
+++ python/agents/main.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+from python.agents.multi_agents import MultiAgentSystem
+from python.data import Generator
+
+
+if __name__ == "__main__":
+    N_MEDICAMENTS = 100
+    N_AGENTS = 10
+    N_ITER = 80
+    COLLABORATIF = True
+
+    sma = MultiAgentSystem(
+        medicaments=Generator.generate_medicaments(N_MEDICAMENTS),
+        N_agents=N_AGENTS,
+        collaboratif=COLLABORATIF,
+    )
+
+    final_result, final_cmax = sma.run(num_steps=N_ITER)
+
+    print("\n--- Résultat de la Simulation SMA ---")
+    print(f"Simulation terminée après {N_ITER} étapes.")
+    print(f"Meilleur ordonnancement global trouvé: {final_result}")
+    print(f"Cmax final global: {final_cmax}")
+
+    # --- Affichage du Cmax par agent au fil des itérations ---
+    agent_data = sma.datacollector.get_agent_vars_dataframe()
+    df = agent_data.reset_index()
+    df_pivot = df.pivot(index="Step", columns="AgentID", values="Makespan")
+    df_pivot.plot(figsize=(10, 6))
+    plt.title("Makespan des agents")
+    plt.xlabel("Step")
+    plt.ylabel("Makespan")
+    plt.show()
---
+++ python/agents/recuit_simule.py
@@ -0,0 +1,88 @@
+import copy
+import random
+
+import numpy as np
+from mesa import Agent
+
+from python.voisins import VoisinsManager
+from python.data import Generator
+
+
+class RecuitSimuleAgent(Agent):
+
+    def __init__(
+        self,
+        model,
+        t0=100,
+        alpha=0.95,
+        cycle_length=100,
+        max_iterations=500,
+        collaboratif=False,
+    ):
+        super().__init__(model)
+
+        self.t0 = t0
+        self.alpha = alpha
+        self.collaboratif = collaboratif
+
+        self.order = Generator.generate_medicaments(len(self.model.medicaments))
+        # self.makespan = compute_makespan(self.model.medicaments, self.order)
+        self.makespan = random.random()
+
+        self.temperature = t0
+        self.iterations = 0
+        self.max_iterations = max_iterations
+        self.cycle_iterations = 0
+        self.cycle_length = cycle_length
+
+    """
+    si je suis collaboratif, j'entre en contact avec les autres
+    je vérifie s'il y a mieux que moi, dans ce cas, je recupere le meilleur dans ma population
+    """
+
+    def contact(self):
+        if not self.collaboratif:
+            return
+
+        for agent in self.model.agents:
+            if agent != self and agent.makespan < self.makespan:
+                self.order = copy.deepcopy(agent.order)
+                self.makespan = agent.makespan
+                break
+
+    def run_iteration(self):
+        if self.iterations >= self.max_iterations:
+            return False
+
+        ordre_voisin = VoisinsManager.generer_voisin(self.order)
+        # makespan_voisin = compute_makespan(self.model.medicaments, ordre_voisin)
+        makespan_voisin = random.random()
+
+        delta = makespan_voisin - self.makespan
+
+        if delta < 0:
+            self.order = ordre_voisin
+            self.makespan = makespan_voisin
+
+        else:
+            proba = np.exp(-delta / self.temperature)
+            if random.random() < proba:
+                self.order = ordre_voisin
+                self.makespan = makespan_voisin
+
+        self.iterations += 1
+        self.cycle_iterations += 1
+
+        if self.cycle_iterations >= self.cycle_length:
+            self.temperature = self.alpha * self.temperature
+            self.cycle_iterations = 0
+
+        return True
+
+    def step(self):
+        still_active = self.run_iteration()
+
+        if self.collaboratif:
+            self.contact()
+
+        return still_active
