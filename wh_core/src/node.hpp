28277513388e1cc63a54ae5fbad91176716d117b
EmericBraud <ebraud95@gmail.com>
2026-02-12 13:07:56 +0100

    added const keywords

--- wh_core/src/node.hpp
+++ wh_core/src/node.hpp
@@ -16,12 +16,12 @@ struct Node
     int id;
     std::unordered_map<int, Link> links;
 
-    bool operator==(const Node &other)
+    bool operator==(const Node &other) const
     {
         return id == other.id;
     }
 
-    int get_distance_from(const Node &other)
+    int get_distance_from(const Node &other) const
     {
         if (other == *this)
             return NODE_DISTANCE_CODE::SAME_NODE;
